e8ac80db5336a153315a46c2fc55c31cce703781
Mythrehe <mythu2124@gmail.com>
2025-02-23 10:37:57 +0530

    Add files via upload

---
+++ prblm 1.py
@@ -0,0 +1,6 @@
+integer = 2
+string = "name"
+character ='y'
+print(integer)
+print(string)
+print(character)
---
+++ prblm 2.py
@@ -0,0 +1,7 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if(a+b+c == 180):
+    print("triangle can be formed")
+else:
+    print("Triangle cannot be formed")
