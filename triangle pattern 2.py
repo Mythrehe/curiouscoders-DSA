489c04cd3eebe548cac8ce4d390b8e641332a109
Mythrehe <mythu2124@gmail.com>
2025-02-26 19:09:29 +0530

    Add files via upload

---
+++ triangle pattern 2.py
@@ -0,0 +1,14 @@
+n=int(input())
+for i in range(1,n):
+    for j in range(1,n-i+1):
+        print(end=" ")
+    for j in range(1,2*i):
+        print("*",end="")
+    print(" ")
+for i in range(1,n+1):
+    for j in range(1,i):
+        print(end=" ")
+    for j in range(1,(2*n)-(2*i)+2):
+        print("*",end="")
+    print()
+
