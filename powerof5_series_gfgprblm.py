eb669424502e1708c2e6b220981e1fabf8bbec61
Mythrehe <mythu2124@gmail.com>
2025-03-08 23:47:56 +0530

    Create powerof5_series_gfgprblm.py

---
+++ powerof5_series_gfgprblm.py
@@ -0,0 +1,10 @@
+#https://www.geeksforgeeks.org/problems/sum-of-fifth-powers-of-the-first-n-natural-numbers3415/1
+
+class Solution:
+    def sumOfFifthPowers(self,N):
+        import math
+        series=0
+        for i in range(1,N+1):
+            sum=pow(i,5)
+            series=series+sum
+        return series
